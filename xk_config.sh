7c2f48dae417e4092946a4f668ca4efeedfa7e1a
tustxk <tustxk@163.com>
2017-09-12 20:34:33 -0700

    add xk_config.sh

---
+++ xk_config.sh
@@ -0,0 +1,10 @@
+#########################################################################
+# File Name: xk_config.sh
+# Author: ke.xiao
+# mail: ke.xiao@netxeon.com
+# Created Time: Tue 12 Sep 2017 08:32:53 PM PDT
+#########################################################################
+#!/bin/bash
+
+sudo cp repo /usr/bin/repo
+sudo chmod a+x /usr/bin/repo
